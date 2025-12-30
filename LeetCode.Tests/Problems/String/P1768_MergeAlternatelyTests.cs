72c8fe2d78c7ae73c15306f49bdc441336f52f61
Marco-Schneider <marco.schneider452@gmail.com>
2025-12-30 00:00:25 -0300

    test(string): add test cases to MergeAlternately solution

---
+++ LeetCode.Tests/Problems/String/P1768_MergeAlternatelyTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Core.Problems.String;
+
+namespace LeetCode.Tests.Problems.String
+{
+    public class P1768_MergeAlternatelyTests
+    {
+
+        [Theory(DisplayName = "Should merge word1 and word2 alternating their characters")]
+        [InlineData("abc", "pqr", "apbqcr")]
+        [InlineData("ab", "pqrs", "apbqrs")]
+        [InlineData("abcd", "pq", "apbqcd")]
+        [InlineData("cdf", "a", "cadf")]
+        public void MergeAlternatelyTest(string word1, string word2, string expectedOutput)
+        {
+            // Arrange
+            var solver = new P1768_MergeAlternately();
+
+            // Act
+            var solution = solver.MergeAlternately(word1, word2);
+
+            // Assert
+            Assert.Equal(expectedOutput, solution);
+        }
+    }
+}
