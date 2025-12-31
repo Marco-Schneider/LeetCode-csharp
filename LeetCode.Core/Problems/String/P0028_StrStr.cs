5d4cd7437fe7b7c823054d8b8d26f79e994d9b18
Marco-Schneider <marco.schneider452@gmail.com>
2025-12-30 23:17:41 -0300

    feat(string): solution for StrStr leveraging the built-in functionality IndexOf

---
+++ LeetCode.Core/Problems/String/P0028_StrStr.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Core.Problems.String
+{
+    public class P0028_StrStr
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            return haystack.IndexOf(needle);
+        }
+    }
+}
