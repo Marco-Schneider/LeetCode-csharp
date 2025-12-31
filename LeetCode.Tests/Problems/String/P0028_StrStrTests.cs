58d85cacd38f68b534d1f3a310bebc2810400b95
Marco-Schneider <marco.schneider452@gmail.com>
2025-12-30 23:18:06 -0300

    test(string): add test cases for StrStr solution

---
+++ LeetCode.Tests/Problems/String/P0028_StrStrTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Core.Problems.String;
+
+namespace LeetCode.Tests.Problems.String
+{
+    public class P0028_StrStrTests
+    {
+        [Theory(DisplayName = "Should return the index if substring is found, otherwise should return -1")]
+        [InlineData("sadbutsad", "sad", 0)]
+        [InlineData("leetcode", "leeto", -1)]
+        public void StrStrTests(string haystack, string needle, int expectedResult)
+        {
+            // Arrange
+            var solver = new P0028_StrStr();
+
+            // Act
+            var solution = solver.StrStr(haystack, needle);
+
+            // Assert
+            Assert.Equal(expectedResult, solution);
+        }
+    }
+}
