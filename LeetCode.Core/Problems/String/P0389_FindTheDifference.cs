022edb4bdc370121e99be72baf8a6ac8d44e1af9
Marco-Schneider <marco.schneider452@gmail.com>
2025-12-30 23:07:31 -0300

    feat(string): solution for FindTheDifference relying on ASCII arithmetic

---
+++ LeetCode.Core/Problems/String/P0389_FindTheDifference.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Core.Problems.String
+{
+    public class P0389_FindTheDifference
+    {
+        public char FindTheDifference(string s, string t)
+        {
+            int originalSize = 0;
+            int finalSize = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                originalSize += s[i];
+            }
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                finalSize += t[i];
+            }
+
+            var addedCharacter = finalSize - originalSize;
+
+            return (char)addedCharacter;
+        }
+    }
+}
