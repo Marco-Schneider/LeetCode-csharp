fd7238227ee2d5f6d9f725bd95255f7633c6aeba
Marco-Schneider <marco.schneider452@gmail.com>
2026-01-12 21:09:05 -0300

    feat(string): solution for IsAnagram counting char frequencies

---
+++ LeetCode.Core/Problems/String/P0242_IsAnagram.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCode.Core.Problems.String
+{
+    public class P0242_IsAnagram
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if(s.Length != t.Length) 
+                return false;
+
+            Dictionary<char, int> frequenciesOfFirstString = [];
+            Dictionary<char, int> frequenciesOfSecondString = [];
+    
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!frequenciesOfFirstString.TryGetValue(s[i], out int _))
+                {
+                    frequenciesOfFirstString.Add(s[i], 1);
+                }
+                else 
+                {
+                    frequenciesOfFirstString[s[i]] += 1;
+                }
+
+                if (!frequenciesOfSecondString.TryGetValue(t[i], out int _))
+                {
+                    frequenciesOfSecondString.Add(t[i], 1);
+                }
+                else {
+                    frequenciesOfSecondString[t[i]] += 1;
+                }
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!frequenciesOfSecondString.TryGetValue(s[i], out int _))
+                {
+                    return false;
+                }
+
+                if (frequenciesOfSecondString[s[i]] != frequenciesOfFirstString[s[i]])
+                {
+                    return false;
+                }
+
+            }
+
+            return true;
+        }
+    }
+}
