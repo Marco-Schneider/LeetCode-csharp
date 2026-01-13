0f89dbc9fc4cbab0b468718643bcfa54ac5aeeeb
Marco-Schneider <marco.schneider452@gmail.com>
2026-01-12 21:09:45 -0300

    test(string): add test cases for IsAnagram solution

---
+++ LeetCode.Tests/Problems/String/P0242_IsAnagramTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Core.Problems.String;
+
+namespace LeetCode.Tests.Problems.String
+{
+    public class P0242_IsAnagramTests
+    {
+        [Theory(DisplayName = "Should correctly determine if the test string t is an anagram of s")]
+        [InlineData("anagram", "nagaram", true)]
+        [InlineData("rat", "car", false)]
+        [InlineData("ggii", "eekk", false)]
+        [InlineData("aacc", "ccac", false)]
+        public void IsAnagramTests(string s, string t, bool expectedResult)
+        {
+            // Arrange
+            var solver = new P0242_IsAnagram();
+
+            // Act
+            var solution = solver.IsAnagram(s, t);
+
+            // Assert
+            Assert.Equal(expectedResult, solution);
+        }
+    }
+}
