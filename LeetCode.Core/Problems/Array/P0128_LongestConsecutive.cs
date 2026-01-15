3fa0fc82824249a93872ea5dc55c0c3588d46f3a
Marco-Schneider <marco.schneider452@gmail.com>
2026-01-14 21:37:52 -0300

    feat(array): solution for LongestConsecutive using HashSet
    Performance can be improved roughly on the 43% percentile

---
+++ LeetCode.Core/Problems/Array/P0128_LongestConsecutive.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Core.Problems.Array
+{
+    public class P0128_LongestConsecutive
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            HashSet<int> numsSet = new(nums);
+
+            int longestSequence = 0;
+            foreach (int number in numsSet)
+            {
+                var currentNumber = number;
+                var sequenceCount = 0;
+                if (numsSet.Contains(number) && !numsSet.Contains(number - 1))
+                {
+                    while (numsSet.Contains(currentNumber))
+                    {
+                        numsSet.Remove(currentNumber);
+                        sequenceCount++;
+                        currentNumber++;
+                    }
+                    
+                    if (sequenceCount > longestSequence)
+                        longestSequence = sequenceCount;
+                }
+            }
+
+            return longestSequence;
+        }
+    }
+}
